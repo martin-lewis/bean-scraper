67b4f89db8b3c398e4d4148c247fd8771e8335cd
Martin Lewis <martin.lewis4321@gmail.com>
2019-12-30 23:06:23 +0000

    Download method written

--- WebUtilities.py
+++ WebUtilities.py
@@ -1,1 +1,19 @@
 #File to contain relevant interfaces for the bean-scraper file and accessing the web
+import urllib3
+
+#Takes a url and a filepath and downloads the file to that filepath
+#Note the file path must contain the filetype, runs from root folder (i.e. location of scripts)
+#Could this be done with a stream? Would that be more effective?
+def downloadFile(url, filepath):
+    print("Downloading --") #Start message, should contain info on what is being downloaded?
+    http = urllib3.PoolManager() #Starts the PoolManager
+    r = http.request('GET', url) #'gets' the file
+    if (r.status == 200): #If the status is good
+        file = open(filepath, 'wb') #Creates the file
+        file.write(r.data) #Writes the data to the file
+        file.close #Closes the file
+        print("Download Complete") #Ending message
+        return True
+    else:
+        print("Error in download, status code: " + str(r.status)) #If it fails, error code returned
+        return False
--- bean-scraper.py
+++ bean-scraper.py
@@ -3,3 +3,4 @@ import FileUtilities as fileUtil
 import WebUtilities as webUtil
 
 print("Starting Bean Scraper") #Test line
+webUtil.downloadFile("https://traffic.libsyn.com/secure/yogpod/Triforce_109_-_Postcode_Killah.mp3?dest-id=25170", "test3.mp3")

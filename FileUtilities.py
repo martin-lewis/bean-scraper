4dc193e03d9db4fa588e5d384ef8f357bc76ac2d
Martin Lewis <martin.lewis4321@gmail.com>
2019-12-31 14:08:26 +0000

    Initial program loop
    The main interaction with the user is carried out here. The loop allowing the user to select the options available

--- FileUtilities.py
+++ FileUtilities.py
@@ -1,4 +1,6 @@
 #File to contain relevant interfaces for the bean-scraper file and the file system of the machine it runs on
+import os
+
 #Checks to see if a file exists
 #Very simple for now as it just calls the os method but it might need to be expanded later hence the interface
 def checkFile(filepath):
--- bean-scraper.py
+++ bean-scraper.py
@@ -3,4 +3,25 @@ import FileUtilities as fileUtil
 import WebUtilities as webUtil
 
 print("Starting Bean Scraper") #Test line
-webUtil.downloadFile("https://traffic.libsyn.com/secure/yogpod/Triforce_109_-_Postcode_Killah.mp3?dest-id=25170", "test3.mp3")
+#webUtil.downloadFile("https://traffic.libsyn.com/secure/yogpod/Triforce_109_-_Postcode_Killah.mp3?dest-id=25170", "test3.mp3")
+#Main program loop
+while True:
+    print("Welcome to Bean Scraper\nPlease select an option")
+    selected = False
+    while not(selected):
+        print("1 - Update Podcasts\n2 - Add a Podcast\n3 - Remove a Podcast\nQ - Quit")
+        response = input("Select an option\n")
+        if (response == "1"):
+            print("Not implimented")
+            selected = True
+        elif (response == "2"):
+            print("Not implimented")
+            selected = True
+        elif (response == "3"):
+            print("Not implimented")
+            selected = True
+        elif ((response == "Q") | (response == "q")):
+            quit()
+        else:
+            print("Not a valid option")
+    #break

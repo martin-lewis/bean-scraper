919ffa34b49c692cacbae7b9bb3fb15c15143fb0
Martin Lewis <martin.lewis4321@gmail.com>
2020-01-01 17:50:24 +0000

    Draft updater
    First version of a scraping system for the updating of a podcast. Takes the url and a name and then gets the rss, scrapes it and then downloads the files not already downloaded

--- bean-scraper.py
+++ bean-scraper.py
@@ -4,10 +4,38 @@ import WebUtilities as webUtil
 import os
 import time
 
-print("Starting Bean Scraper") #Test line
-#webUtil.downloadFile("https://traffic.libsyn.com/secure/yogpod/Triforce_109_-_Postcode_Killah.mp3?dest-id=25170", "test3.mp3")
-print(fileUtil.checkDir(".rss"))
+##Functions
 
+#Takes a url of a rss feed and updates the local files
+def updatePodcast(rssUrl, podcastName): #Podcast name is in use for a place to put the file, the actual name is in the XML
+    #Step one: Get the rss feed downloaded
+    print("Downloading rss file for: " + podcastName)
+    webUtil.downloadFile(rssUrl, (".rss/" + podcastName))
+    #Open the file
+    rss = open(".rss/" + podcastName)
+    #Look through each line for an audio file
+    urls =[] #List for found urls
+    for line in rss: #for each line
+        result = fileUtil.fileUrlScraper(line) #Check for file urls
+        if (result != None): #If a result is returned then
+            urls.append(result) #Added to list
+    #Make sure there is a folder to hold them
+    podcastFolderExists = fileUtil.checkDir(podcastName)
+    if (not(podcastFolderExists)):
+        os.mkdir(podcastName)
+    #Downloads the files
+    for url in urls:
+        filename = fileUtil.findFileName(url) #Find the file name from the url
+        fileAlreadyExists = fileUtil.checkFile(podcastName + "/" + filename) #Check if it has already been downloaded
+        if (not(fileAlreadyExists)): #If not
+            print("Fetching new file: " + filename)
+            webUtil.downloadFile(url, podcastName + "/" + filename) #Download the file to the correct location
+        else:
+            print("file exists:" + filename) #If its already downloaded then nothing happens
+
+##Main
+
+print("Starting Bean Scraper") #Test line
 
 #Start up operations before start
 #Data structure and variable creation
@@ -27,11 +55,19 @@ else: #If there is one it needs to read it
         feeds.append(splitLine[0]) #Gets the RSS which is in the first position of each line
     feedsFile.close()
 
+#Rss folder
+feedsFolderExists = fileUtil.checkDir(".rss")
+if (not(feedsFolderExists)):
+    print("Making folder")
+    os.mkdir(".rss")
+
 """ Test lines
 print("Current feeds:")
 for feed in feeds:
     print(feed)
 """
+#Test rss download
+t = updatePodcast("https://anchor.fm/s/cd90d44/podcast/rss", "Qu")
 
 #Main program loop
 while True:

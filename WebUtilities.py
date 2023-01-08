38362664b0b57383a179e6b224284751442b72c3
Martin Lewis <martin.lewis4321@gmail.com>
2023-01-08 17:11:06 +0000

    Updating License Dates

--- WebUtilities.py
+++ WebUtilities.py
@@ -1,6 +1,6 @@
 """
     Bean Scraper - Web Utilities
-    Copyright (C) 2019-2020  Martin Lewis
+    Copyright (C) 2019-2023  Martin Lewis
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
